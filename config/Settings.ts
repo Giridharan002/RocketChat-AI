1dc21bb7cfabf93c10d17eba3e7e684ac30b17c0
Giridharan002 <giri.2004k@gmail.com>
2024-04-17 21:34:36 +0530

    configured settings

---
+++ config/Settings.ts
@@ -0,0 +1,21 @@
+import {
+    ISetting,
+    SettingType,
+} from "@rocket.chat/apps-engine/definition/settings";
+
+export enum AppSetting {
+    NAMESPACE = "",
+    GENAI_STACK_API_URL = "http://localhost:8504/query-stream"
+}
+
+export const settings: Array<ISetting> = [
+    {
+        id: AppSetting.GENAI_STACK_API_URL,
+        public: true,
+        type: SettingType.STRING,
+        packageValue: "",
+        hidden: false,
+        i18nLabel: AppSetting.NAMESPACE + "GENAI_STACK_API_URL",
+        required: true,
+    },
+];
